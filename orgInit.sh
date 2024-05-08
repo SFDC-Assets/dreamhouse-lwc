f620df2c98d001ff0f6c9eeeb7cfe660a40a7c8d
Mikun Adewole <mikun.adewole@mikunad-ltmrdvk.internal.salesforce.com>
2024-05-08 18:04:29 -0400

    heythere

---
+++ orgInit.sh
@@ -0,0 +1,7 @@
+#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --wait 60
+sfdx force:source:push
+sf org assign permset -n dreamhouse
+sf org assign permset -n Walkthroughs
+sf data tree import -p data/sample-data-plan.json
+sfdx force:source:open
