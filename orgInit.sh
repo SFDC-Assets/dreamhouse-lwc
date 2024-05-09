38f6edd9354c0ab40682d1a7ea14ed7fe063166d
Mikun Adewole <mikun.adewole@mikunad-ltmrdvk.internal.salesforce.com>
2024-05-09 15:47:56 -0400

    Added old commands in place of new ones

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,8 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
 #sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --wait 60
 sfdx force:source:push
-sf org assign permset -n dreamhouse
-sf org assign permset -n Walkthroughs
-sf data tree import -p data/sample-data-plan.json
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:user:permset:assign -n Walkthroughs
+sfdx force:data:tree:import -p data/sample-data-plan.json
+sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:source:open
