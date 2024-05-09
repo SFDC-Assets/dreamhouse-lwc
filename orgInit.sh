f3e43ac40a1e2abf43ba175e3fe8162d61e4e51c
Mikun Adewole <mikun.adewole@mikunad-ltmrdvk.internal.salesforce.com>
2024-05-09 15:41:10 -0400

    Change from force to shane

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,5 @@
-#sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --wait 60
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
+#sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --wait 60
 sfdx force:source:push
 sf org assign permset -n dreamhouse
 sf org assign permset -n Walkthroughs
