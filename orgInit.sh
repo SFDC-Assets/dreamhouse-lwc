1576650dca68b703c28954eba63eb6265793a595
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-05-13 21:51:04 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
 sfdx shane:user:password:set -p salesforce1 -g User -l User
-# sfdx force:source:push
-# sfdx force:user:permset:assign -n dreamhouse
-# sfdx force:user:permset:assign -n Walkthroughs
-# sfdx force:data:tree:import -p data/sample-data-plan.json
+sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:user:permset:assign -n Walkthroughs
+sfdx force:data:tree:import -p data/sample-data-plan.json
 sfdx force:source:open
