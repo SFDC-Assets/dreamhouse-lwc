767bf2310fe9dc96be0912e973596b0913bf25a8
zhou059 <zhou059@gmail.com>
2024-05-14 14:12:16 -0700

    more

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,8 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
 sfdx force:source:push
+sfdx force:user:permset:assign -n dreamhouse
+sfdx force:user:permset:assign -n Walkthroughs
+sfdx force:data:tree:import -p data/sample-data-plan.json
+
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:org:open
-
