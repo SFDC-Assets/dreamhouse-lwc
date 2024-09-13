7cf9ac16b55a1aadaec8c1189555a1f537d39cfb
zhou059 <zhou059@gmail.com>
2024-09-13 13:20:39 -0700

    init

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix dream -o house.demo
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -s --wait 60
 
 npm install
 node run.js
@@ -9,5 +9,4 @@ sfdx force:user:permset:assign -n dreamhouse
 sfdx force:user:permset:assign -n Walkthroughs
 sfdx force:data:tree:import -p data/sample-data-plan.json
 
-sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:org:open
